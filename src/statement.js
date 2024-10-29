b496ac599617ef2549c644a02af97da41eac076b
232295311 <232295311@qq.com>
2024-10-29 23:16:26 +0800

    拆分阶段，想要拆成不同主题的两个模块，开始转移invoice

--- src/statement.js
+++ src/statement.js
@@ -1,14 +1,16 @@
 export default function statement(invoice, plays) {
   let statementData = {};
+  statementData.customer = invoice.customer;
   return renderPlainText(statementData, invoice, plays);
 }
 
 // 拆分阶段（154），将大段代码拆分成各自独立的模块，每个模块单独维护一个主题。
 // 现在我想拆成两个模块，第一模块负责计算，第二模块负责打印。
 // 第一步，先提炼全部函数到第二模块的函数中
-// 第二部，增加中转数据结构data，接下来的目的是逐渐将invoice 和 plays 转移到 data 中
+// 第二步，增加中转数据结构data，接下来的目的是逐渐将invoice 和 plays 转移到 data 中
+// 第三步，转移invoice.customer
 function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `Statement for ${data.customer}\n`;
   for (let perf of invoice.performances) {
     // print line for this order
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
