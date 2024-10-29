aac12bf544c9a720c67bf6446731de1acd205382
232295311 <232295311@qq.com>
2024-10-29 22:55:15 +0800

    提炼函数+内联变量，移除变量totalAmount

--- src/statement.js
+++ src/statement.js
@@ -1,19 +1,22 @@
 export default function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
-  let totalAmount = 0;
-  for (let perf of invoice.performances) {
-    totalAmount += amountFor(perf);
-  }
-
   for (let perf of invoice.performances) {
     // print line for this order
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function totalVolumeCredits() {
     let result = 0;
