86158ad23087a0cfa7fb86667f7ed2369f2c2263
232295311 <232295311@qq.com>
2024-10-29 23:39:46 +0800

    拆分阶段，想要拆成不同主题的两个模块，转移amountFor

--- src/statement.js
+++ src/statement.js
@@ -6,53 +6,9 @@ export default function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
+
     return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  return renderPlainText(statementData);
-}
-
-// 拆分阶段（154），将大段代码拆分成各自独立的模块，每个模块单独维护一个主题。
-// 现在我想拆成两个模块，第一模块负责计算，第二模块负责打印。
-// 第一步，先提炼全部函数到第二模块的函数中
-// 第二步，增加中转数据结构data，接下来的目的是逐渐将invoice 和 plays 转移到 data 中
-// 第三步，转移invoice.customer
-// 第四步，转移invoice.performances，消除invoice参数
-// 第五步，打算转移plays，需要先转移playFor，新增map函数
-// 第六步，转移plays成功，消除plays参数
-function renderPlainText(data) {
-  let result = `Statement for ${data.customer}\n`;
-  for (let perf of data.performances) {
-    // print line for this order
-    result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-  }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-  return result;
-
-  // ================ 计算相关的逻辑都在下面的函数中 ================
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber / 100);
   }
 
   function amountFor(aPerformance) {
@@ -75,6 +31,52 @@ function renderPlainText(data) {
         throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  return renderPlainText(statementData);
+}
+
+// 拆分阶段（154），将大段代码拆分成各自独立的模块，每个模块单独维护一个主题。
+// 现在我想拆成两个模块，第一模块负责计算，第二模块负责打印。
+// 第一步，先提炼全部函数到第二模块的函数中
+// 第二步，增加中转数据结构data，接下来的目的是逐渐将invoice 和 plays 转移到 data 中
+// 第三步，转移invoice.customer
+// 第四步，转移invoice.performances，消除invoice参数
+// 第五步，打算转移plays，需要先转移playFor，新增map函数
+// 第六步，转移plays成功，消除plays参数
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let perf of data.performances) {
+    // print line for this order
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+  }
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;
+
+  // ================ 计算相关的逻辑都在下面的函数中 ================
+  function totalAmount() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber / 100);
   }
 
   function volumeCreditsFor(aPerformance) {
