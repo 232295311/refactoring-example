586054fe1c36db8c182ecb46c20fb499417f84fc
232295311 <232295311@qq.com>
2024-10-29 21:42:43 +0800

    变量重命名，永远将函数的返回值命名为result（风格）;参数取名时，带上类型名和不定冠词（风格）

--- src/statement.js
+++ src/statement.js
@@ -1,25 +1,25 @@
 // 提炼函数（106），如果你需要花时间浏览一段代码才能弄清它到底在干什么，那么就应该提炼它。
 // 根据它做的事为它命名， 以后再读到这段代码的时候，一眼就能看出函数的用途，而不需要关心它如何达成该用途。
-function amountFor(perf, play) {
-  let thisAmount = 0;
+function amountFor(aPerformance, play) {
+  let result = 0;
   switch (play.type) {
     case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
 export default function statement(invoice, plays) {
   let totalAmount = 0;
