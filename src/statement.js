75f10054389f0d4a5c5232c66f06985cfc4c4d5f
232295311 <232295311@qq.com>
2024-10-29 23:12:57 +0800

    拆分阶段，想要拆成不同主题的两个模块，再增加中转数据结构

--- src/statement.js
+++ src/statement.js
@@ -1,21 +1,22 @@
 export default function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  let statementData = {};
+  return renderPlainText(statementData, invoice, plays);
 }
 
 // 拆分阶段（154），将大段代码拆分成各自独立的模块，每个模块单独维护一个主题。
 // 现在我想拆成两个模块，第一模块负责计算，第二模块负责打印。
 // 第一步，先提炼全部函数到第二模块的函数中
-function renderPlainText(invoice, plays) {
+// 第二部，增加中转数据结构data，接下来的目的是逐渐将invoice 和 plays 转移到 data 中
+function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
-
   for (let perf of invoice.performances) {
     // print line for this order
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
-
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
   // ================ 计算相关的逻辑都在下面的函数中 ================
   function totalAmount() {
     let result = 0;
