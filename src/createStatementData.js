74cfe1134ba84fff8711e2f41df4069009d31505
232295311 <232295311@qq.com>
2024-10-30 15:48:13 +0800

    多态: 迁移amount条件表达式到comedy子类

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -17,12 +17,7 @@ class PerformanceCalculator {
       case 'tragedy':
         throw new Error('bad thing');
       case 'comedy':
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
+        throw new Error('bad bad thing');
       default:
         throw new Error(`unknown type: ${this.play.type}`);
     }
@@ -40,7 +35,7 @@ class PerformanceCalculator {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
-  // 迁移条件表达式，原使用处抛出错误。
+  // 迁移条件表达式
   get amount() {
     let result = 40000;
     if (this.performance.audience > 30) {
@@ -50,7 +45,17 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  // 迁移条件表达式
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+}
 
 // 以工厂函数取代构造函数（334）
 function createPerformanceCalculator(aPerformance, aPlay) {
